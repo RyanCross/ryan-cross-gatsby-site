11be168a2ec4daea75c948b0ca4b5a607f77072a
Ryan Cross <ryan.cross.home@gmail.com>
2021-10-23 19:27:19 -0500

    forgot to commit new file

---
+++ src/pages/hireme.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const HireMe = () => {
+    return (
+        <main>
+            <h1> Give me a job you freakin jag! </h1>
+            <Link to="/">Home</Link>
+        </main>
+        
+    )
+}
+
+export default HireMe
