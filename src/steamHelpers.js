296707d079f9cf713ab4efe31234315fa5fec4b4
Ryan Cross <ryan.cross.home@gmail.com>
2023-03-28 14:25:53 -0500

    remove headers entirely

--- src/steamHelpers.js
+++ src/steamHelpers.js
@@ -8,17 +8,17 @@ export async function getGameInfoForPlayer(steamId) {
   let data;
   try {
     const requestConfig = {
-        headers: {
-            // "Origin": 'http:127.0.0.1/8000',
-            // "Access-Control-Request-Method": "GET",
-            "Access-Control-Allow-Origin": "*"
-        }
+        // headers: {
+        //     // "Origin": 'http:127.0.0.1/8000',
+        //     // "Access-Control-Request-Method": "GET",
+        //     "Access-Control-Allow-Origin": "*"
+        // }
     //   headers: {
     //     "Access-Control-Allow-Origin": "*",
     //     "Content-Type": "application/x-www-form-urlencoded"
     //   },
     };
-    let response = await axios.get(url, requestConfig);
+    let response = await axios.get(url);
     data = await response.json();
   } catch (error) {
     console.log("hi");
